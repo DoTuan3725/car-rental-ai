af98880daea959c073c47d59e99008ef43f0036e
tuanda <tuanda05@gmai.com>
2026-09-20 03:07:27 +0700

    feat: implement car CRUD use cases with license plate uniqueness check

---
+++ src/CarRental.Application/Modules/Cars/ICarService.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Http;
+
+namespace CarRental.Application.Modules.Cars;
+
+public interface ICarService
+{
+    Task<(List<CarDto> Items, int TotalCount)> GetFilteredAsync(CarFilterRequest filter);
+    Task<CarDto?> GetByIdAsync(int id);
+    Task<CarDto> CreateAsync(CreateCarRequest request);
+    Task UpdateAsync(int id, UpdateCarRequest request);
+    Task DeleteAsync(int id);
+    Task<CarImageDto> AddImageAsync(int carId, IFormFile file);
+    Task SetPrimaryImageAsync(int carId, int imageId);
+}
