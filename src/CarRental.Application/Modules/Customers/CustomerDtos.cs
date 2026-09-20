e369b4fd2c9363c8175fa520883a2678333ebb26
tuanda <tuanda05@gmai.com>
2026-09-20 21:38:19 +0700

    Remove property to fix postman bug

--- src/CarRental.Application/Modules/Customers/CustomerDtos.cs
+++ src/CarRental.Application/Modules/Customers/CustomerDtos.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 namespace CarRental.Application.Modules.Customers;
 
 public record RegisterRequest(
-    [property: Required] string FullName,
-    [property: Required, EmailAddress] string Email,
-    [property: Required, Phone] string PhoneNumber,
-    [property: Required, MinLength(8)] string Password);
+    [Required] string FullName,
+    [Required, EmailAddress] string Email,
+    [Required, Phone] string PhoneNumber,
+    [Required, MinLength(8)] string Password);
 public record LoginRequest(string Email, string Password);
 public record AuthResponse(string Token, string FullName, string Role);
 public record CustomerDto(int Id, string FullName, string Email, string PhoneNumber, string Role, string? IdentityNumber, string? Address);
